6755f13a41b1e84b23497886657700dcad3441c3
liyanfeng <liyanfeng0601.@163.com>
2019-03-22 23:46:27 +0800

    2019.3.22

--- Chapter9/demo1.py
+++ Chapter9/demo1.py
@@ -1,8 +1,18 @@
-class Dog():
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
-    def sit(self):
-        print(self.name+"is sitting")f
-    def roll(self):
-        print(self.age+"is rolling")
+import time
+import threading
+def sing():
+    for i in range(5):
+        print("sing********************")
+        time.sleep(1)
+def dance():
+    for i in range(5):
+        print("dance+++++++++++++++++++")
+        time.sleep(1)
+def main():
+    t1 = threading.Thread(target=sing)
+    t2 = threading.Thread(target=dance)
+    t1.start()
+    t2.start()
+if __name__ == "__main__":
+    main()
+
