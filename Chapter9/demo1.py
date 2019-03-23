066306bd61e8a6a3b7c6c959878204d14a71b354
liyanfeng <liyanfeng0601.@163.com>
2019-03-23 10:00:59 +0800

    modfiy

--- Chapter9/demo1.py
+++ Chapter9/demo1.py
@@ -9,10 +9,20 @@ def dance():
         print("dance+++++++++++++++++++")
         time.sleep(1)
 def main():
-    t1 = threading.Thread(target=sing)
+    # 创建子线程对象
+    t1 = threading.Thread(target=sing) #函数名（）；调用函数     函数名；指定函数的位置
     t2 = threading.Thread(target=dance)
     t1.start()
+    time.sleep(1)
     t2.start()
+    time.sleep(1)
+  #  while True:
+   #     for tmp in threading.enumerate():  #线程数
+     #       if threading.enumerate() < 0:
+      #          print ("over")
+      #          break
+       #     else:
+    print (threading.enumerate())
 if __name__ == "__main__":
     main()
 
