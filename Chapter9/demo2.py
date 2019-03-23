0f734af426e9fe60b436aff4376eb585ad3a9922
liyanfeng <liyanfeng0601.@163.com>
2019-03-23 10:06:00 +0800

    默认

---
+++ Chapter9/demo2.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+import threading
+import time
+class MyThread(threading.Thread):
+    def run(self):
+        for i in range(3):
+            time.sleep(1)
+            msg = "I'am"+self.name+"@"+str(i)
+            print(msg)
+if __name__ == "__main__":
+    t=MyThread()
+    t.start() #此处用start方法  会直接运行 run
