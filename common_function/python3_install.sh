aa2670f70e1eb37d060a13f299b33497e238c05e
liyanfeng <liyanfeng0601@163.com>
2019-04-29 18:19:34 -0400

    modify

--- common_function/python3_install.sh
+++ common_function/python3_install.sh
@@ -2,5 +2,5 @@
 echo "*****************start install pyhon3****************"
 echo "************we nedd to install python url is******************"
 
-wget_url= ${PYTHON_URL}/${PYTHON_VERSION}
+wget_url= "${PYTHON_URL}/${PYTHON_VERSION}"
 wget ${wget_url}
