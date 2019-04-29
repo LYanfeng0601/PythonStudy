4d11ac002eb32c9285d68ade0f3a678ea4149307
liyanfeng <liyanfeng0601@163.com>
2019-04-29 18:35:22 -0400

    modify

--- common_function/python3_install.sh
+++ common_function/python3_install.sh
@@ -3,5 +3,11 @@ echo ${PYTHON_URL}
 export ${PYTHON_VERSION}
 export work_dir=/usr1/ci_work
 mkdir -p ${work_dir}
-cd {work_dir}
+cd ${work_dir}
 wget ${PYTHON_URL}/${PYTHON_VERSION}
+if [ "$?"=="0" ];then
+	echo "wget success"
+else
+	echo "wget fail"
+	exit 1
+fi
