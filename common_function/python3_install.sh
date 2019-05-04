0fd036d0b002530ef0cf7fe0a030f49d89e0c45c
liyanfeng <liyanfeng0601@163.com>
2019-05-04 05:56:57 -0400

    modify

--- common_function/python3_install.sh
+++ common_function/python3_install.sh
@@ -1,9 +1,10 @@
-#coding=utf-8
+#!/bin/bash
 echo ${PYTHON_URL}
 export ${PYTHON_VERSION}
 export work_dir=/usr1/ci_work
 mkdir -p ${work_dir}
 cd ${work_dir}
+yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gcc make
 wget ${PYTHON_URL}/${PYTHON_VERSION}
 if [ "$?"=="0" ];then
 	echo "wget success"
@@ -11,3 +12,9 @@ else
 	echo "wget fail"
 	exit 1
 fi
+tar xf ${PYTHON_VERSION}
+cd Python-3*
+./configure prefix=/usr/local/python3
+make && make install
+ln -s /usr/local/python3/bin/python3 /usr/bin/python
+python --version
