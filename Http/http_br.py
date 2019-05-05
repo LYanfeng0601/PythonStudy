03b2d39fec36cb28c75a1138b972c3f8fc5df43a
liyanfeng <liyanfeng0601.@163.com>
2019-05-05 21:53:48 +0800

    add

--- Http/http_br.py
+++ Http/http_br.py
@@ -3,14 +3,14 @@ import socket
 
 def deal_with(sock_request,addr_request):
 	while True:
-	recv_data = sock_request.recv(1204)
-	#send_data = input("please input your words")
-	send_data = "<h1>hello world</h1>"
-	if recv_data:
-		print(recv_data.decode("utf-8"))
-		sock_request.send(send_data.encode("utf-8"))
-	else:
-		break
+		recv_data = sock_request.recv(1204)
+		#send_data = input("please input your words")
+		send_data = "<h1>hello world</h1>"
+		if recv_data:
+			print(recv_data.decode("utf-8"))
+			sock_request.send(send_data.encode("utf-8"))
+		else:
+			break
 
 def main():
 	tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
