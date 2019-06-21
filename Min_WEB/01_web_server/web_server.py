153e2e7d3f6b42039e46e14c4bceb096e38d5d5a
liyanfeng <liyanfeng0601@163.com>
2019-06-21 15:32:20 -0400

    add web_server.py

--- Min_WEB/01_web_server/web_server.py
+++ Min_WEB/01_web_server/web_server.py
@@ -1,76 +1,54 @@
-# coding=UTF-8
-import socket
+#coding-"UTF-8"
+import time
 import re
+import socket
 import multiprocessing
 
 class WSGIServer(object):
 	def __init__(self):
-		## 1.创建套接字
-		self.tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.tcp_server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 		self.tcp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
-		#2. 绑定端口
-		self.tcp_server_socket.bind(("", 7890))
-		# 3. 变为监听套接字
+		self.tcp_server_socket.bind(('192.168.0.106',7890))
 		self.tcp_server_socket.listen(128)
 
-	def service_client(self,new_socket):
-	    """为这个客户端返回数据"""
-	    # 1. 接收浏览器发送过来的请求 ，即http请求  
-	    # GET / HTTP/1.1
-	    # .....
-		#request = new_socket.recv(1024).decode("utf-8")
+	def server_client(self,new_socket):
+		print(new_socket)
+		request = new_socket.recv(1024).decode("utf-8")	
 		request_lines = request.splitlines()
-		print("")
-		print(">"*20)
-		print(request_lines)
-		# GET /index.html HTTP/1.1
-		# get post put del
 		file_name = ""
 		ret = re.match(r"[^/]+(/[^ ]*)", request_lines[0])
 		if ret:
+			html_content = ''
 			file_name = ret.group(1)
-		# print("*"*50, file_name)
 			if file_name == "/":
-		    		file_name = "/index.html"
-	    		# 2. 返回http格式的数据，给浏览器
-		try:
-			f = open("./html" + file_name, "rb")
-		except:
-			response = "HTTP/1.1 404 NOT FOUND\r\n"
-			response += "\r\n"
-			response += "------file not found-----"
-			new_socket.send(response.encode("utf-8"))
-	    	else:
-			html_content = f.read()
-			f.close()
-		# 2.1 准备发送给浏览器的数据---header
-			response = "HTTP/1.1 200 OK\r\n"
-			response += "\r\n"
-		# 2.2 准备发送给浏览器的数据---boy
-		# response += "hahahhah"
-
-		# 将response header发送给浏览器
-			new_socket.send(response.encode("utf-8"))
-		# 将response body发送给浏览器
-			new_socket.send(html_content)
-
-
-	    # 关闭套接
-		new_socket.close()
-	    
+				file_name = "/index.html"
+			try:
+				f = open("./html" + file_name, "rb")
+			except:
+				response = "HTTP/1.1 404 NOT FOUND\r\n"
+				response += "\r\n"
+				response += "------file not found-----"
+				new_socket.send(response.encode("utf-8"))
+			else:
+				html_content = f.read()
+				f.close()
+				response = "HTTP/1.1 200 OK\r\n"
+				response += "\r\n"
+				new_socket.send(response.encode("utf-8"))
+				new_socket.send(html_content)
+			new_socket.close()
+	
 	def run_forever(self):
-	    #"""用来完成整体的控制"""
 		while True:
-			# 4. 等待新客户端的链接
-			new_socket, client_addr = self.tcp_server_socket.accept()
-			# 5. 为这个客户端服务
-			p = multiprocessing.Process(target=self.service_client,args=(new_socket,))
+			new_socket,new_addr = self.tcp_server_socket.accept()
+			p = multiprocessing.Process(target=self.server_client,args=(new_socket,))
 			p.start()
 			new_socket.close()
-		self.tcp_server_socket()
+		self.tcp_server_socket.close()
+
 def main():
-    wsgi_server = WSGIServer()
-    wsgi_server.run_forever()
+	wsgi_server = WSGIServer()
+	wsgi_server.run_forever()
 
 if __name__ == "__main__":
 	main()
