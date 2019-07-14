0b1c5adc2bba57408f7d4d6a8b2ce07d427c8b21
liyanfeng <liyanfeng0601.@163.com>
2019-07-14 19:14:03 +0800

    modify

--- Min_WEB/update/04-从数据库查询数据/dynamic/mini_frame.py
+++ Min_WEB/update/04-从数据库查询数据/dynamic/mini_frame.py
@@ -68,10 +68,39 @@ def index():
 def center():
     with open("./templates/center.html") as f:
         content = f.read()
+            conn = connect(host='192.168.0.103', port=3306, user='root',
+                           password='root', database='mysql', charset='utf8')
+    # 获得Cursor对象
+    cs = conn.cursor()
+    cs.execute("select  i.code,i.chg,i.turnover,i.highs,f.note_info from infor as i inner join focus as f on i.id=f.info_id;")
+    stock_infos = cs.fetchall()
+    cs.close()
+    conn.close()
 
+    tr_template = """
+        <tr>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td>20</td>
+            <td>30</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td>
+                <a type="button" class="btn btn-default btn-xs" href="/update/300268.html"> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> 修改 </a>
+            </td>
+            <td>
+                <input type="button" value="删除" id="toDel" name="toDel" systemidvaule="300268">
+            </td>
+        </tr>
+    """
+    html = ''
+    for line_info in stock_infos:
+        html += str_template % (line_info[0], line_info[1], line_info[2], line_info[
+                                3], line_info[4],line_info[5])
     my_stock_info = "这里是从mysql查询出来的数据。。。"
 
-    content = re.sub(r"\{%content%\}", my_stock_info, content)
+    content = re.sub(r"\{%content%\}", str(html), content)
 
     return content
 
